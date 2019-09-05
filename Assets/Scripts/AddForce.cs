dfec90643b45c06503d69fc072901886bd598730
Frédéric Rolland-Porché <frederic@rolland-porche.fr>
2019-09-05 19:55:30 +0200

    - NEW : force added from input system, inspector

---
+++ Assets/Scripts/AddForce.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AddForce : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody body;
+    [SerializeField]
+    float strength = 10;
+    [SerializeField]
+    float coolDown = 0.25f;
+    [SerializeField]
+    LineRenderer line;
+    [SerializeField]
+    Transform coneTransform;
+
+    Transform target;
+    float nextTime;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        nextTime = 0;
+        target = GameObject.FindGameObjectWithTag("target").transform;
+    }
+
+    public void OnForce(InputAction.CallbackContext context)
+    {
+        ApplyForce();
+    }
+
+    private void Update()
+    {
+        // editor arrow handle cap reproduced at runtime
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, transform.position + body.velocity);
+        // Move the cone at the top
+        coneTransform.position = transform.position + body.velocity;
+        coneTransform.rotation = Quaternion.LookRotation(body.velocity);
+    }
+
+    void ApplyForce()
+    {
+        if (Time.time > nextTime)
+        {
+            nextTime = Time.time + coolDown;
+            Vector3 direction = (target.position - transform.position).normalized;
+            body.AddForce(direction * strength, ForceMode.Impulse);
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Editor/AddForceInspector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AddForce))]
+public class AddForceInspector : Editor
+{
+    const float sizeRatio = 0.1f;
+
+    private void OnEnable()
+    {
+        SceneView.duringSceneGui += OnScene;
+    }
+
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= OnScene;
+    }
+
+    private void OnScene(SceneView sceneview)
+    {
+        Rigidbody body = (this.target as MonoBehaviour).GetComponent<Rigidbody>();
+        Handles.color = Color.yellow;
+        // Draw an arrow handler from the rigidbody velocity (representing the result from all combined forces)
+        // we constraint the ball on the XY plane and the camera is backward from the ball
+        Handles.ArrowHandleCap(0, body.transform.position, Quaternion.LookRotation(body.velocity, Vector3.back), 
+                                    body.velocity.sqrMagnitude * sizeRatio, EventType.Repaint);
+    }
+}
